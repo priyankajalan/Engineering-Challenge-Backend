f541af599da4a50a1f00497e0b78a31f86741ae6
priyankajalan <priyankajalan1991@gmail.com>
2015-07-10 17:49:36 +0800

    Scrapperjs comments updated
    Updated the comments in scrapperjs

--- client/scrapper.js
+++ client/scrapper.js
@@ -1,5 +1,6 @@
 'use strict';
 
+//requiring the modules
 var request = require('request'),
 	cheerio = require('cheerio'),
 	urls = [],
@@ -10,8 +11,11 @@ request('http://www.myfitnesspal.com/food', function(error, response, body){
 	if(!error && response.statusCode == 200){
 		var $ = cheerio.load(body);
 		
+		//selecting the class food_description and anchor element
 		$('.food_description a').each(function(){
+			//selecting the text of the element selected
 			var url = $(this).text();
+			//pushing the data to the urls array
 			urls.push(url);
 		});
 		
@@ -25,11 +29,15 @@ request('http://www.myfitnesspal.com/food/calories/homemade-chicken-salad-fillin
 	if(!error && response.statusCode == 200){
 		var $ = cheerio.load(body);
 		
+		//selecting the id value nutrition-facts and the td element
 		$('#nutrition-facts td').each(function(){
+			//selecting the text of the element selected
 			var nutrition = $(this).text();
+			//pushing the data to the nutritions array
 			nutritions.push(nutrition);
 		});
 		
+		//display the nutritions array data in the console
 		console.log(nutritions);
 
 	}
